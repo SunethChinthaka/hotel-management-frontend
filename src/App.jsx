0c8b7f0bf49edd76c286f481f2182f35a2b54cde
SunethChinthaka <sunethedu@gmail.com>
2024-02-12 12:50:06 +0530

    App.jsx and index.css modified

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import Home from "./components/home/Home";
 import EditRoom from "./components/rooms/EditRoom";
 import NavBar from "./components/layouts/NavBar";
 import Footer from "./components/layouts/Footer";
+import RoomListing from "./components/rooms/RoomListing";
+import Admin from "./components/admin/Admin";
 
 function App() {
   return (
@@ -21,6 +23,8 @@ function App() {
             <Route path="/edit-room/:roomId" element={<EditRoom />} />
             <Route path="/existing-rooms" element={<ExistingRooms />} />
             <Route path="/add-room" element={<AddRoom />} />
+            <Route path="/browse-all-rooms" element={<RoomListing />} />
+            <Route path="/admin" element={<Admin />} />
           </Routes>
         </Router>
         <Footer />
