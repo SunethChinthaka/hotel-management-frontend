70d6f880f4f39f07fc6b4e716d52ec4e32fc0412
SunethChinthaka <sunethedu@gmail.com>
2024-02-12 12:48:16 +0530

    Home Added

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,31 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import MainHeader from "../layouts/MainHeader";
+import Parallax from "../common/Parallax";
+import HotelService from "../common/HotelService";
+import RoomCarousel from "../common/RoomCarousel";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section>
+      {/* {message && <p className="text-warning px-5">{message}</p>}
+      {currentUser && (
+        <h6 className="text-success text-center">
+          {" "}
+          You are logged-In as {currentUser}
+        </h6>
+      )} */}
+      <MainHeader />
+      <section className="container">
+        <RoomCarousel />
+        <Parallax />
+        <RoomCarousel />
+        <HotelService />
+        <Parallax />
+        <RoomCarousel />
+      </section>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
