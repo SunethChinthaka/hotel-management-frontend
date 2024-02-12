e6740c511e42aafbd6a227b7ea9ed59cec4648b9
SunethChinthaka <sunethedu@gmail.com>
2024-02-12 20:01:55 +0530

    bookings-BookingForm, BookingSuccess, BookingSummary, Checkout Components added

---
+++ src/components/bookings/Checkout.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import BookingForm from "./BookingForm";
+
+const Checkout = () => {
+  return (
+    <div>
+      <BookingForm />
+    </div>
+  );
+};
+
+export default Checkout;
