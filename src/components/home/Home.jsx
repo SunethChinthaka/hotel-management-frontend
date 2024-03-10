971b5ad6fce6ae6df32405b18c32d63989bdbd81
SunethChinthaka <sunethedu@gmail.com>
2024-03-10 22:56:23 +0530

    RoomSearch and RoomSearchResults implemented

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,6 +4,7 @@ import MainHeader from "../layouts/MainHeader";
 import Parallax from "../common/Parallax";
 import HotelService from "../common/HotelService";
 import RoomCarousel from "../common/RoomCarousel";
+import RoomSearch from "../common/RoomSearch";
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
       )} */}
       <MainHeader />
       <section className="container">
+        <RoomSearch />
         <RoomCarousel />
         <Parallax />
         <RoomCarousel />
